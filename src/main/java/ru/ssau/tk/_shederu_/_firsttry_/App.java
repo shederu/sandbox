618a19a7299ec311e816e8072250e701b8e64aad
shederu <lagereva.lis@gmail.com>
2025-09-22 15:55:59 +0400

    изменила хеллоу ворлд

--- src/main/java/ru/ssau/tk/_shederu_/_firsttry_/App.java
+++ src/main/java/ru/ssau/tk/_shederu_/_firsttry_/App.java
@@ -3,6 +3,6 @@ package ru.ssau.tk._shederu_._firsttry_;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! когда же все уже заработает...");
     }
 }
