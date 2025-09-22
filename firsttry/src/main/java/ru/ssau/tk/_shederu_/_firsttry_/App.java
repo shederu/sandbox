9f328e3df79b3db54cc96af269bb55ffde0904d6
shederu <lagereva.lis@gmail.com>
2025-09-22 16:11:39 +0400

    Новенькое

---
+++ firsttry/src/main/java/ru/ssau/tk/_shederu_/_firsttry_/App.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk._shederu_._firsttry_;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
